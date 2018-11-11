2f9695c13c42de5387d3b0332a4083bb4ad326fe
Vedant Chaudhari <v.chaudhari@jagoworld.org>
2018-11-11 17:17:30 -0500

    Added Circular Player Movement, Prorotyped basic stuff

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Transform mTarget;
+
+    private float mStep = 0.2f;
+    private Vector3 mCenter;
+	private float mRadius = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+        mCenter = mTarget.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		getInput();
+	}
+
+	/* PRIVATE FUNCTIONS */
+	private void getInput() {
+		if (SystemInfo.deviceType == DeviceType.Desktop) {
+			if (Input.GetKey(KeyCode.RightArrow))
+				Rotate(true);
+			else if (Input.GetKey(KeyCode.LeftArrow)) {
+				Rotate(false);
+			}
+		}
+		else {	// SETUP FOR MOBILE CONTROLS
+
+		}
+	}
+	private Vector2 PointOnCircle(float angle) {
+		float x = (mRadius * Mathf.Cos(angle * Mathf.PI / 180.0f) + mCenter.x);
+		float y = (mRadius * Mathf.Cos(angle * Mathf.PI / 180.0f) + mCenter.y);
+
+		return new Vector2(x, y);
+	}
+
+	// True for right, False for left
+	private void Rotate(bool direction) {
+		if (direction)
+			transform.RotateAround(mCenter, new Vector3(0, 0.5f, 0), 60 * Time.deltaTime);
+		else
+			transform.RotateAround(mCenter, -new Vector3(0, 0.5f, 0), 60 * Time.deltaTime);
+	}
+}
